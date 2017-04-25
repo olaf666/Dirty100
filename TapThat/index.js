0310626dad851ea7ed9b2e54bc28dc7896deac8c
olaf666 <ayesha14.1.97@gmail.com>
2017-04-25 16:58:29 +0530

    Add files via upload

---
+++ TapThat/index.js
@@ -0,0 +1,594 @@
+$(document).ready(function() {
+	function randomcolor() {
+		return '#' + Math.random().toString(16).slice(2,8);
+	};
+	$(".square").on("mouseenter",function() {
+		$("body").css("background-color",randomcolor());
+		$(this).css("background-color",randomcolor());
+	 
+	});
+$(function() {
+	var box1hover = $("#box1");
+	var box1audio = box1hover.find("audio")[0];
+
+	box1hover.hover(
+		function() {
+		box1audio.play();
+	},
+		function() {
+			box1audio.stop();
+		});
+	});
+
+$(function() {
+	var box2hover = $("#box2");
+	var box2audio = box2hover.find("audio")[0];
+
+	box2hover.hover(
+		function() {
+		box2audio.play();
+	},
+		function() {
+			box2audio.stop();
+		});
+	});
+
+$(function() {
+	var box3hover = $("#box3");
+	var box3audio = box3hover.find("audio")[0];
+
+	box3hover.hover(
+		function() {
+		box3audio.play();
+	},
+		function() {
+			box3audio.stop();
+		});
+	});
+
+$(function() {
+	var box4hover = $("#box4");
+	var box4audio = box4hover.find("audio")[0];
+
+	box4hover.hover(
+		function() {
+		box4audio.play();
+	},
+		function() {
+			box4audio.stop();
+		});
+	});
+
+$(function() {
+	var box5hover = $("#box5");
+	var box5audio = box5hover.find("audio")[0];
+
+	box5hover.hover(
+		function() {
+		box5audio.play();
+	},
+		function() {
+			box5audio.stop();
+		});
+	});
+
+$(function() {
+	var box6hover = $("#box6");
+	var box6audio = box6hover.find("audio")[0];
+
+	box6hover.hover(
+		function() {
+		box6audio.play();
+	},
+		function() {
+			box6audio.stop();
+		});
+	});
+
+$(function() {
+	var box7hover = $("#box7");
+	var box7audio = box7hover.find("audio")[0];
+
+	box7hover.hover(
+		function() {
+		box7audio.play();
+	},
+		function() {
+			box7audio.stop();
+		});
+	});
+
+$(function() {
+	var box8hover = $("#box8");
+	var box8audio = box8hover.find("audio")[0];
+
+	box8hover.hover(
+		function() {
+		box8audio.play();
+	},
+		function() {
+			box8audio.stop();
+		});
+	});
+
+$(function() {
+	var box9hover = $("#box9");
+	var box9audio = box9hover.find("audio")[0];
+
+	box9hover.hover(
+		function() {
+		box9audio.play();
+	},
+		function() {
+			box9audio.stop();
+		});
+	});
+
+$(function() {
+	var box10hover = $("#box10");
+	var box10audio = box10hover.find("audio")[0];
+
+	box10hover.hover(
+		function() {
+		box10audio.play();
+	},
+		function() {
+			box10audio.stop();
+		});
+	});
+
+$(function() {
+	var box11hover = $("#box11");
+	var box11audio = box11hover.find("audio")[0];
+
+	box11hover.hover(
+		function() {
+		box11audio.play();
+	},
+		function() {
+			box11audio.stop();
+		});
+	});
+
+$(function() {
+	var box12hover = $("#box12");
+	var box12audio = box12hover.find("audio")[0];
+
+	box12hover.hover(
+		function() {
+		box12audio.play();
+	},
+		function() {
+			box12audio.stop();
+		});
+	});
+
+$(function() {
+	var box13hover = $("#box13");
+	var box13audio = box13hover.find("audio")[0];
+
+	box13hover.hover(
+		function() {
+		box13audio.play();
+	},
+		function() {
+			box13audio.stop();
+		});
+	});
+
+$(function() {
+	var box14hover = $("#box14");
+	var box14audio = box14hover.find("audio")[0];
+
+	box14hover.hover(
+		function() {
+		box14audio.play();
+	},
+		function() {
+			box14audio.stop();
+		});
+	});
+
+$(function() {
+	var box15hover = $("#box15");
+	var box15audio = box15hover.find("audio")[0];
+
+	box15hover.hover(
+		function() {
+		box15audio.play();
+	},
+		function() {
+			box15audio.stop();
+		});
+	});
+
+$(function() {
+	var box16hover = $("#box16");
+	var box16audio = box16hover.find("audio")[0];
+
+	box16hover.hover(
+		function() {
+		box16audio.play();
+	},
+		function() {
+			box16audio.stop();
+		});
+	});
+
+$(function() {
+	var box17hover = $("#box17");
+	var box17audio = box17hover.find("audio")[0];
+
+	box17hover.hover(
+		function() {
+		box17audio.play();
+	},
+		function() {
+			box17audio.stop();
+		});
+	});
+
+$(function() {
+	var box18hover = $("#box18");
+	var box18audio = box18hover.find("audio")[0];
+
+	box18hover.hover(
+		function() {
+		box18audio.play();
+	},
+		function() {
+			box18audio.stop();
+		});
+	});
+
+$(function() {
+	var box19hover = $("#box19");
+	var box19audio = box19hover.find("audio")[0];
+
+	box19hover.hover(
+		function() {
+		box19audio.play();
+	},
+		function() {
+			box19audio.stop();
+		});
+	});
+
+$(function() {
+	var box20hover = $("#box20");
+	var box20audio = box20hover.find("audio")[0];
+
+	box20hover.hover(
+		function() {
+		box20audio.play();
+	},
+		function() {
+			box20audio.stop();
+		});
+	});
+
+$(function() {
+	var box21hover = $("#box21");
+	var box21audio = box21hover.find("audio")[0];
+
+	box21hover.hover(
+		function() {
+		box21audio.play();
+	},
+		function() {
+			box21audio.stop();
+		});
+	});
+
+$(function() {
+	var box22hover = $("#box22");
+	var box22audio = box22hover.find("audio")[0];
+
+	box22hover.hover(
+		function() {
+		box22audio.play();
+	},
+		function() {
+			box22audio.stop();
+		});
+	});
+
+$(function() {
+	var box23hover = $("#box23");
+	var box23audio = box23hover.find("audio")[0];
+
+	box23hover.hover(
+		function() {
+		box23audio.play();
+	},
+		function() {
+			box23audio.stop();
+		});
+	});
+
+$(function() {
+	var box24hover = $("#box24");
+	var box24audio = box24hover.find("audio")[0];
+
+	box24hover.hover(
+		function() {
+		box24audio.play();
+	},
+		function() {
+			box24audio.stop();
+		});
+	});
+
+$(function() {
+	var box25hover = $("#box25");
+	var box25audio = box25hover.find("audio")[0];
+
+	box25hover.hover(
+		function() {
+		box25audio.play();
+	},
+		function() {
+			box25audio.stop();
+		});
+	});
+
+$(function() {
+	var box26hover = $("#box26");
+	var box26audio = box26hover.find("audio")[0];
+
+	box26hover.hover(
+		function() {
+		box26audio.play();
+	},
+		function() {
+			box26audio.stop();
+		});
+	});
+
+$(function() {
+	var box27hover = $("#box27");
+	var box27audio = box27hover.find("audio")[0];
+
+	box27hover.hover(
+		function() {
+		box27audio.play();
+	},
+		function() {
+			box27audio.stop();
+		});
+	});
+
+$(function() {
+	var box28hover = $("#box28");
+	var box28audio = box28hover.find("audio")[0];
+
+	box28hover.hover(
+		function() {
+		box28audio.play();
+	},
+		function() {
+			box28audio.stop();
+		});
+	});
+
+$(function() {
+	var box29hover = $("#box29");
+	var box29audio = box29hover.find("audio")[0];
+
+	box29hover.hover(
+		function() {
+		box29audio.play();
+	},
+		function() {
+			box29audio.stop();
+		});
+	});
+
+$(function() {
+	var box30hover = $("#box30");
+	var box30audio = box30hover.find("audio")[0];
+
+	box30hover.hover(
+		function() {
+		box30audio.play();
+	},
+		function() {
+			box30audio.stop();
+		});
+	});
+
+$(function() {
+	var box31hover = $("#box31");
+	var box31audio = box31hover.find("audio")[0];
+
+	box31hover.hover(
+		function() {
+		box31audio.play();
+	},
+		function() {
+			box31audio.stop();
+		});
+	});
+
+$(function() {
+	var box32hover = $("#box32");
+	var box32audio = box32hover.find("audio")[0];
+
+	box32hover.hover(
+		function() {
+		box32audio.play();
+	},
+		function() {
+			box32audio.stop();
+		});
+	});
+
+$(function() {
+	var box33hover = $("#box33");
+	var box33audio = box33hover.find("audio")[0];
+
+	box33hover.hover(
+		function() {
+		box33audio.play();
+	},
+		function() {
+			box33audio.stop();
+		});
+	});
+
+$(function() {
+	var box34hover = $("#box34");
+	var box34audio = box34hover.find("audio")[0];
+
+	box34hover.hover(
+		function() {
+		box34audio.play();
+	},
+		function() {
+			box34audio.stop();
+		});
+	});
+
+$(function() {
+	var box35hover = $("#box35");
+	var box35audio = box35hover.find("audio")[0];
+
+	box35hover.hover(
+		function() {
+		box35audio.play();
+	},
+		function() {
+			box35audio.stop();
+		});
+	});
+
+$(function() {
+	var box36hover = $("#box36");
+	var box36audio = box36hover.find("audio")[0];
+
+	box36hover.hover(
+		function() {
+		box36audio.play();
+	},
+		function() {
+			box36audio.stop();
+		});
+	});
+
+$(function() {
+	var box37hover = $("#box37");
+	var box37audio = box37hover.find("audio")[0];
+
+	box37hover.hover(
+		function() {
+		box37audio.play();
+	},
+		function() {
+			box37audio.stop();
+		});
+	});
+
+$(function() {
+	var box38hover = $("#box38");
+	var box38audio = box38hover.find("audio")[0];
+
+	box38hover.hover(
+		function() {
+		box38audio.play();
+	},
+		function() {
+			box38audio.stop();
+		});
+	});
+
+$(function() {
+	var box39hover = $("#box39");
+	var box39audio = box39hover.find("audio")[0];
+
+	box39hover.hover(
+		function() {
+		box39audio.play();
+	},
+		function() {
+			box39audio.stop();
+		});
+	});
+
+$(function() {
+	var box40hover = $("#box40");
+	var box40audio = box40hover.find("audio")[0];
+
+	box40hover.hover(
+		function() {
+		box40audio.play();
+	},
+		function() {
+			box40audio.stop();
+		});
+	});
+
+$(function() {
+	var box41hover = $("#box41");
+	var box41audio = box41hover.find("audio")[0];
+
+	box41hover.hover(
+		function() {
+		box41audio.play();
+	},
+		function() {
+			box41audio.stop();
+		});
+	});
+
+$(function() {
+	var box42hover = $("#box42");
+	var box42audio = box42hover.find("audio")[0];
+
+	box42hover.hover(
+		function() {
+		box42audio.play();
+	},
+		function() {
+			box42audio.stop();
+		});
+	});
+
+$(function() {
+	var box43hover = $("#box43");
+	var box43audio = box43hover.find("audio")[0];
+
+	box43hover.hover(
+		function() {
+		box43audio.play();
+	},
+		function() {
+			box43audio.stop();
+		});
+	});
+
+$(function() {
+	var box44hover = $("#box44");
+	var box44audio = box44hover.find("audio")[0];
+
+	box44hover.hover(
+		function() {
+		box44audio.play();
+	},
+		function() {
+			box44audio.stop();
+		});
+	});
+
+$(function() {
+	var box45hover = $("#box45");
+	var box45audio = box45hover.find("audio")[0];
+
+	box45hover.hover(
+		function() {
+		box45audio.play();
+	},
+		function() {
+			box45audio.stop();
+		});
+	});
+});
